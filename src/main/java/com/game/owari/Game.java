da3744456fbf9c9e7cbc8d77d9e69584a8c01662
KarowM <karow.foxx@gmail.com>
2018-08-08 19:10:10 +0100

    swap player in new game changes current player to player 2

--- src/main/java/com/game/owari/Game.java
+++ src/main/java/com/game/owari/Game.java
@@ -1,7 +1,18 @@
 package com.game.owari;
 
 public class Game {
+    private int currentPlayer;
+
+    public Game()
+    {
+        currentPlayer = 1;
+    }
+
     public int getCurrentPlayer() {
-        return 1;
+        return currentPlayer;
+    }
+
+    public void swapPlayers() {
+        currentPlayer = 2;
     }
 }
--- src/test/java/com/game/owari/TestGame.java
+++ src/test/java/com/game/owari/TestGame.java
@@ -1,15 +1,30 @@
 package com.game.owari;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestGame {
 
-    private static Game game = new Game();
+    private static Game game;
+
+    @Before
+    public void setup()
+    {
+        game = new Game();
+    }
 
     @Test
     public void gameStartsWithPlayerOne()
     {
-        Assert.assertEquals(game.getCurrentPlayer(), 1);
+        Assert.assertEquals(1, game.getCurrentPlayer());
+    }
+
+    @Test
+    public void swapPlayerSwitchesToPlayerTwo()
+    {
+        game.swapPlayers();
+
+        Assert.assertEquals(2, game.getCurrentPlayer());
     }
 }
