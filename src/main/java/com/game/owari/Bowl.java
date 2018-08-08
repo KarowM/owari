0aa7e7317f96d658480d77518a6fd98f8b59c448
KarowM <karow.foxx@gmail.com>
2018-08-08 18:18:52 +0100

    bowl created with args has specified number of balls

--- src/main/java/com/game/owari/Bowl.java
+++ src/main/java/com/game/owari/Bowl.java
@@ -1,7 +1,17 @@
 package com.game.owari;
 
 public class Bowl {
+    private final int numOfBalls;
+
+    public Bowl(int initialBalls) {
+        numOfBalls = initialBalls;
+    }
+
+    public Bowl() {
+        this(0);
+    }
+
     public int getStones() {
-        return 0;
+        return numOfBalls;
     }
 }
--- src/test/java/com/game/owari/TestBowl.java
+++ src/test/java/com/game/owari/TestBowl.java
@@ -1,17 +1,31 @@
 package com.game.owari;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestBowl {
 
     private static Bowl BOWL;
 
+    @Before
+    public void setup()
+    {
+        BOWL = new Bowl();
+    }
+
     @Test
     public void bowlCreationWithNoArgsCreatesEmptyBowl()
     {
-        BOWL = new Bowl();
-
         Assert.assertEquals(BOWL.getStones(), 0);
+    }
+
+    @Test
+    public void bowlCreationWithArgsCreatesBowlWithSpecifiedNumberOfBalls()
+    {
+        final int INITIAL_BALLS = 1;
+        BOWL = new Bowl(INITIAL_BALLS);
+
+        Assert.assertEquals(BOWL.getStones(), INITIAL_BALLS);
     }
 }
